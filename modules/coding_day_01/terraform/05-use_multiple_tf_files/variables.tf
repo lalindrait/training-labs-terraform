59f051ffc5cfef4e9577175c4d8504451aa6e224
lalindrait <57055160+lalindrait@users.noreply.github.com>
2024-05-31 16:39:26 +0530

    change day1 order

---
+++ modules/coding_day_01/terraform/05-use_multiple_tf_files/variables.tf
@@ -0,0 +1,7 @@
+variable "ec2_name" {
+    default = "msd-ec2"
+} 
+
+variable "ec2_owner" {
+    default = "MSD"
+} 
--- modules/coding_day_01/terraform/08-use_multiple_tf_files/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "ec2_name" {
-    default = "msd-ec2"
-} 
-
-variable "ec2_owner" {
-    default = "MSD"
-} 
